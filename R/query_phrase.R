1a652c775cc70472c520dcf6f0955b13bf0bc2d9
Meera Patel <patelm9@mskcc.org>
2020-04-22 20:22:10 -0400

    add case_insensitive option to query_phrase functions

--- R/query_phrase.R
+++ R/query_phrase.R
@@ -8,27 +8,53 @@
 #' @export
 
 query_phrase <-
-        function(phrase, limit = NULL, type = c("like", "exact")) {
+        function(phrase, limit = NULL, type = c("like", "exact"), case_insensitive = TRUE) {
+            if (case_insensitive == FALSE) {
                 if (type == "exact") {
-                        if (is.null(limit)) {
-                                sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name = '", phrase, "';")
-                        } else {
-                                sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name = '", phrase, "' ")
-                                sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
-                        }
-
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name = '", phrase, "';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name = '", phrase, "' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
+                    
                 } else if (type == "like") {
-                        if (is.null(limit)) {
-                                sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name LIKE '%", phrase, "%';")
-                        } else {
-                                sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name LIKE '%", phrase, "%' ")
-                                sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
-                        }
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name LIKE '%", phrase, "%';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE concept_name LIKE '%", phrase, "%' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
                 }
-
+                
                 conn_to_athena <- mySeagull::connect_to_local_postgres(dbname = "athena")
                 resultset <- DBI::dbGetQuery(conn = conn_to_athena,
                                              statement = sql_statement)
                 DBI::dbDisconnect(conn_to_athena)
                 return(resultset)
+            } else {
+                if (type == "exact") {
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE LOWER(concept_name) = '", tolower(phrase), "';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE LOWER(concept_name) = '", tolower(phrase), "' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
+                    
+                } else if (type == "like") {
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE LOWER(concept_name) LIKE '%", tolower(phrase), "%';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept WHERE LOWER(concept_name) LIKE '%", tolower(phrase), "%' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
+                }
+                
+                conn_to_athena <- mySeagull::connect_to_local_postgres(dbname = "athena")
+                resultset <- DBI::dbGetQuery(conn = conn_to_athena,
+                                             statement = sql_statement)
+                DBI::dbDisconnect(conn_to_athena)
+                return(resultset)
+            }
+              
         }
--- R/query_phrase_synonym.R
+++ R/query_phrase_synonym.R
@@ -8,27 +8,53 @@
 #' @export
 
 query_phrase_synonym <-
-        function(phrase, limit = NULL, type = c("like", "exact")) {
+        function(phrase, limit = NULL, type = c("like", "exact"), case_insensitive = TRUE) {
+            if (case_insensitive == FALSE) {
                 if (type == "exact") {
-                        if (is.null(limit)) {
-                                sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name = '", phrase, "';")
-                        } else {
-                                sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name = '", phrase, "' ")
-                                sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
-                        }
-
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name = '", phrase, "';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name = '", phrase, "' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
+                    
                 } else if (type == "like") {
-                        if (is.null(limit)) {
-                                sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name LIKE '%", phrase, "%';")
-                        } else {
-                                sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name LIKE '%", phrase, "%' ")
-                                sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
-                        }
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name LIKE '%", phrase, "%';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE concept_synonym_name LIKE '%", phrase, "%' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
                 }
-
+                
                 conn_to_athena <- mySeagull::connect_to_local_postgres(dbname = "athena")
                 resultset <- DBI::dbGetQuery(conn = conn_to_athena,
                                              statement = sql_statement)
                 DBI::dbDisconnect(conn_to_athena)
                 return(resultset)
+            } else {
+                if (type == "exact") {
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE LOWER(concept_synonym_name) = '", tolower(phrase), "';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE LOWER(concept_synonym_name) = '", tolower(phrase), "' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
+                    
+                } else if (type == "like") {
+                    if (is.null(limit)) {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE LOWER(concept_synonym_name) LIKE '%", tolower(phrase), "%';")
+                    } else {
+                        sql_statement <- paste0("SELECT * FROM public.concept_synonym WHERE LOWER(concept_synonym_name) LIKE '%", tolower(phrase), "%' ")
+                        sql_statement <- paste0(sql_statement, "LIMIT ", limit, ";")
+                    }
+                }
+                
+                conn_to_athena <- mySeagull::connect_to_local_postgres(dbname = "athena")
+                resultset <- DBI::dbGetQuery(conn = conn_to_athena,
+                                             statement = sql_statement)
+                DBI::dbDisconnect(conn_to_athena)
+                return(resultset)
+            }
+               
         }
