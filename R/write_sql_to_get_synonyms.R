00659fd8caa852a8de22681a66d4c6c367503c90
Meera Patel <patelm9@mskcc.org>
2020-04-28 13:21:31 -0400

    add language parameter in write_sql_to_get_synonyms function

--- R/write_sql_to_get_synonyms.R
+++ R/write_sql_to_get_synonyms.R
@@ -3,10 +3,14 @@
 #' @export
 
 write_sql_to_get_synonyms <-
-        function(concept_id) {
+        function(concept_id, language_concept_id = c("4180186")) {
+            language_concept_id <- paste0("'", language_concept_id, "'")
+            
+            language_concept_ids <- seagull::prepare_vector(language_concept_id)
+            language_concept_ids <- paste0("(", language_concept_ids, ")")
                 sql_statement <-
                         paste0("SELECT *
                                FROM concept_synonym c 
-                               WHERE c.concept_id = '", concept_id, "';")
+                               WHERE c.concept_id = '", concept_id, "' AND c.language_concept_id IN ", language_concept_ids, ";")
                 return(sql_statement)
         }
---
+++ README.md
@@ -0,0 +1,10 @@
+2020-04-28 13:07:55 	File Created
+
+# Typewrite Messages
+Timestamp:	2020-04-28 13:21:31
+Local R Script:	~/GitHub/Public-Packages/chariot/R/write_sql_to_get_synonyms.R
+Typewrite Message:	No deleted files in this repo.
+Timestamp:	2020-04-28 13:07:55
+Local R Script:	~/GitHub/Public-Packages/chariot/R/write_sql_to_get_synonyms.R
+Typewrite Message:	No deleted files in this repo.
+
