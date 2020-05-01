cb2655ec67eb8a586b65d86486f3c5f243ea4741
Meera Patel <patelm9@mskcc.org>
2020-05-01 11:45:24 -0400

    bug fix write_sql_for_descendants function

--- R/write_sql_for_descendants.R
+++ R/write_sql_for_descendants.R
@@ -1,13 +1,14 @@
 #' Write SQL to get OMOP Concept Descendants
 #' @description This function returns the SQL statement that can be used to query on either a cloud or local instance of the OMOP vocabulary.
 #' @param ancestor_concept_id OMOP concept id of the ancestor concept
-#' @param max_levels_of_separation Integer of the maximum levels of separation
+#' @param max_levels_of_separation Optional. Integer of the maximum levels of separation. 
 #' @export
 
 write_sql_for_descendants <-
     function(ancestor_concept_id,
-             max_levels_of_separation) {
+             max_levels_of_separation = NULL) {
         
+            if (!is.null(max_levels_of_separation)) {
                 sql_statement <-
                     paste0(
                         "SELECT * 
@@ -16,6 +17,16 @@ write_sql_for_descendants <-
                         ON concept_id = a.descendant_concept_id 
                         WHERE a.ancestor_concept_id = '", ancestor_concept_id, "' 
                         and max_levels_of_separation = '", max_levels_of_separation, "';")
+            } else {
+                sql_statement <-
+                    paste0(
+                        "SELECT * 
+                        FROM concept_ancestor a
+                        LEFT JOIN concept
+                        ON concept_id = a.descendant_concept_id 
+                        WHERE a.ancestor_concept_id = '", ancestor_concept_id, "';")
+                
+            }
                 
                 return(sql_statement)
     }
