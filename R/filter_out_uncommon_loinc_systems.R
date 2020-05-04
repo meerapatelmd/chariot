9f5195e3fa1693d1dcb6ce67b0157178d0414c56
Meera Patel <patelm9@mskcc.org>
2020-05-04 19:47:00 -0400

    add filter_out_uncommon_loinc_systems function

---
+++ R/filter_out_uncommon_loinc_systems.R
@@ -0,0 +1,15 @@
+#' Filter out uncommon LOINC Systems
+#' @importFrom dplyr enquo
+#' @importFrom rubix filter_at_grepl
+#' @export
+
+filter_out_uncommon_loinc_systems <-
+    function(dataframe, loinc_systems_col) {
+        
+            loinc_systems_col <- dplyr::enquo(loinc_systems_col)
+            dataframe %>%
+            rubix::filter_at_grepl(col = !!loinc_systems_col,
+                                   grepl_phrase = "Donor|XX|Cerebra|Peric|Body|Perito|Platelet poor|Stool|Sediment|Gastric|Synovial|Lymph Node|Pleural",
+                                   evaluates_to = FALSE)
+        
+    }
