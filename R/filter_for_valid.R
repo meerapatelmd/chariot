4fab50d232edf2eff50c4fdb42fa8b6705a02327
Meera Patel <patelm9@mskcc.org>
2020-05-10 18:33:23 -0400

    bug fix filter_for_valid

--- R/filter_for_valid.R
+++ R/filter_for_valid.R
@@ -1,6 +1,7 @@
 #' Returns all possible OMOP domains in Athena
 #' @return dataframe of unique `domain_id` variables from the `concept` table. If `rm_columns` = TRUE, removes all the columns that are irrelevant if all valids are filtered for (date, invalid_reason column itself.)
 #' @importFrom dplyr filter
+#' @importFrom dplyr select
 #' @export
 
 filter_for_valid <-
@@ -8,7 +9,7 @@ filter_for_valid <-
             if (rm_columns == TRUE) {
                 dataframe %>%
                     dplyr::filter(is.na(invalid_reason)) %>%
-                    dplyr::filter(-valid_start_date, -valid_end_date, -invalid_reason)
+                    dplyr::select(-valid_start_date, -valid_end_date, -invalid_reason)
             } else {
                 dataframe %>%
                     dplyr::filter(is.na(invalid_reason)) 
