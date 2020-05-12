cc681a347cb29637668a401f653d1de091189334
Meera Patel <patelm9@mskcc.org>
2020-05-11 23:06:02 -0400

    bug fixes

--- R/plot_concept_hierarchy.R
+++ R/plot_concept_hierarchy.R
@@ -10,101 +10,119 @@
 #' @export
 
 plot_concept_hierarchy <-
-    function(concept_id, generations_before = 2, generations_after = 2) {
+    function(concept_id, generations_before = 0, generations_after = 0) {
         
-        output_descendents <- list()
-        df_a <-
-            query_concept_id(concept_id) %>%
-            merge_concepts("Parent Concept", shorthand = TRUE) %>%
-            dplyr::select(parent_concept_id = concept_id,
-                          `Parent Concept`)
-        df_b <-
-            query_descendants(concept_id, max_levels_of_separation = 1) %>%
-            filter_for_valid() %>%
-            merge_concepts(into = "Child Concept", shorthand = TRUE) %>%
-            dplyr::rename(parent_concept_id = ancestor_concept_id,
-                          child_concept_id = descendant_concept_id) %>%
-            dplyr::select(parent_concept_id, child_concept_id, `Child Concept`)
-        
-        output_descendents[[1]] <-
-            dplyr::left_join(df_a,
-                             df_b)
-        
-        for (i in 1:generations_after) {
-            previous_output <- output_descendents[[i]]
-            next_output_index <- i+1
-            
+        if (generations_after > 0) {
+            output_descendants <- list()
             df_a <-
-                previous_output %>%
-                dplyr::select(parent_concept_id = child_concept_id,
-                              `Parent Concept` = `Child Concept`)
-            
+                query_concept_id(concept_id) %>%
+                merge_concepts("Parent Concept", shorthand = TRUE) %>%
+                dplyr::select(parent_concept_id = concept_id,
+                              `Parent Concept`)
             df_b <-
-                query_descendants(ancestor_concept_ids = df_a$parent_concept_id,
-                                           max_levels_of_separation = 1) %>%
+                query_descendants(concept_id, max_levels_of_separation = 1) %>%
                 filter_for_valid() %>%
                 merge_concepts(into = "Child Concept", shorthand = TRUE) %>%
-                dplyr::rename(child_concept_id = descendant_concept_id,
-                              parent_concept_id = ancestor_concept_id) %>%
-                dplyr::select(parent_concept_id,
-                              child_concept_id,
-                              `Child Concept`)
+                dplyr::rename(parent_concept_id = ancestor_concept_id,
+                              child_concept_id = descendant_concept_id) %>%
+                dplyr::select(parent_concept_id, child_concept_id, `Child Concept`)
             
-            output_descendents[[next_output_index]] <- dplyr::left_join(df_a,
-                                                            df_b) %>%
-                dplyr::distinct()
+            output_descendants[[1]] <-
+                dplyr::left_join(df_a,
+                                 df_b)
             
+            generations_after <- generations_after-1
+            
+            if (generations_after > 1) {
+                for (i in 1:generations_after) {
+                    previous_output <- output_descendants[[i]]
+                    next_output_index <- i+1
+                    
+                    df_a <-
+                        previous_output %>%
+                        dplyr::select(parent_concept_id = child_concept_id,
+                                      `Parent Concept` = `Child Concept`)
+                    
+                    df_b <-
+                        query_descendants(ancestor_concept_ids = df_a$parent_concept_id,
+                                          max_levels_of_separation = 1) %>%
+                        filter_for_valid() %>%
+                        merge_concepts(into = "Child Concept", shorthand = TRUE) %>%
+                        dplyr::rename(child_concept_id = descendant_concept_id,
+                                      parent_concept_id = ancestor_concept_id) %>%
+                        dplyr::select(parent_concept_id,
+                                      child_concept_id,
+                                      `Child Concept`)
+                    
+                    output_descendants[[next_output_index]] <- dplyr::left_join(df_a,
+                                                                                df_b) %>%
+                        dplyr::distinct()
+                    
+                }
+                output_descendants <- dplyr::bind_rows(output_descendants)
+                
+            }
+                
+        } else {
+            output_descendants <- list()
         }
-        output_descendents <- dplyr::bind_rows(output_descendents)
         
-        
-        output_ancestors <- list()
-        df_a <-
-            query_concept_id(concept_id) %>%
-            merge_concepts("Child Concept", shorthand = TRUE) %>%
-            dplyr::select(child_concept_id = concept_id,
-                          `Child Concept`)
-        df_b <-
-            query_ancestors(concept_id, max_levels_of_separation = 1) %>%
-            filter_for_valid() %>%
-            merge_concepts(into = "Parent Concept", shorthand = TRUE) %>%
-            dplyr::rename(parent_concept_id = ancestor_concept_id,
-                          child_concept_id = descendant_concept_id) %>%
-            dplyr::select(parent_concept_id, child_concept_id, `Parent Concept`)
-        
-        output_ancestors[[1]] <-
-            dplyr::left_join(df_a,
-                             df_b)
-        
-        for (i in 1:generations_before) {
-            previous_output <- output_ancestors[[i]]
-            next_output_index <- i+1
-            
+        if (generations_before > 0) {
+            output_ancestors <- list()
             df_a <-
-                previous_output %>%
-                dplyr::select(child_concept_id = parent_concept_id,
-                              `Child Concept` = `Parent Concept`)
-            
+                query_concept_id(concept_id) %>%
+                merge_concepts("Child Concept", shorthand = TRUE) %>%
+                dplyr::select(child_concept_id = concept_id,
+                              `Child Concept`)
             df_b <-
-                query_ancestors(df_a$child_concept_id,
-                                           max_levels_of_separation = 1) %>%
+                query_ancestors(concept_id, max_levels_of_separation = 1) %>%
                 filter_for_valid() %>%
                 merge_concepts(into = "Parent Concept", shorthand = TRUE) %>%
-                dplyr::rename(child_concept_id = descendant_concept_id,
-                              parent_concept_id = ancestor_concept_id) %>%
-                dplyr::select(parent_concept_id,
-                              child_concept_id,
-                              `Parent Concept`)
+                dplyr::rename(parent_concept_id = ancestor_concept_id,
+                              child_concept_id = descendant_concept_id) %>%
+                dplyr::select(parent_concept_id, child_concept_id, `Parent Concept`)
             
-            output_ancestors[[next_output_index]] <- dplyr::left_join(df_a,
-                                                                        df_b) %>%
-                dplyr::distinct()
+            output_ancestors[[1]] <-
+                dplyr::left_join(df_a,
+                                 df_b)
             
+            if (generations_before > 1) {
+                for (i in 1:generations_before) {
+                    previous_output <- output_ancestors[[i]]
+                    next_output_index <- i+1
+                    
+                    df_a <-
+                        previous_output %>%
+                        dplyr::select(child_concept_id = parent_concept_id,
+                                      `Child Concept` = `Parent Concept`)
+                    
+                    df_b <-
+                        query_ancestors(df_a$child_concept_id,
+                                        max_levels_of_separation = 1) %>%
+                        filter_for_valid() %>%
+                        merge_concepts(into = "Parent Concept", shorthand = TRUE) %>%
+                        dplyr::rename(child_concept_id = descendant_concept_id,
+                                      parent_concept_id = ancestor_concept_id) %>%
+                        dplyr::select(parent_concept_id,
+                                      child_concept_id,
+                                      `Parent Concept`)
+                    
+                    output_ancestors[[next_output_index]] <- dplyr::left_join(df_a,
+                                                                              df_b) %>%
+                        dplyr::distinct()
+                    
+                }
+                output_ancestors <- dplyr::bind_rows(output_ancestors)
+                
+            }
+            
+        } else {
+            output_ancestors <- list()
         }
-        output_ancestors <- dplyr::bind_rows(output_ancestors)
+        
         
         output <- dplyr::bind_rows(output_ancestors,
-                                   output_descendents) %>%
+                                   output_descendants) %>%
                     dplyr::filter_all(all_vars(!is.na(.)))  %>%
             dplyr::rename(child = `Child Concept`,
                           parent = `Parent Concept`)
