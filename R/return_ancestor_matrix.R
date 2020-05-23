706c98114a3dc05f6ab0592cfa7d834c0416c368
Meera Patel <patelm9@mskcc.org>
2020-05-23 00:40:40 -0400

    add return_ancestor_matrix concept_ids

--- R/return_ancestor_matrix.R
+++ R/return_ancestor_matrix.R
@@ -1,6 +1,10 @@
 #' Return a Ancestor Matrix
 #' @description This function returns a matrix of ancestors by descendants to view if a group of concepts have any common ancestors 
-#' @importFrom tidyr pivot_wider
+#' @examples 
+#' # Random immunosuppressant concept ids
+#' immunosuppressant_concept_ids <- c("35807335","35807331", "21603616", "21600651", "21605199", "21602723") 
+#' return_ancestor_matrix(immunosuppressant_concept_ids)
+#' @importFrom tidyr pivot_wider 
 #' @export
 
 return_ancestor_matrix <-
