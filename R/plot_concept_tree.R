8c96ff431d017ca87fba165ef3dc2cc3683827eb
Meera Patel <patelm9@mskcc.org>
2020-06-30 17:31:24 -0400

    add cdm function

---
+++ R/plot_concept_tree.R
@@ -0,0 +1,30 @@
+
+# 
+# 
+# dataframe <- tibble(test_concept_id = "4266756")
+# 
+# 
+# plot_concept_tree <- 
+#     function(dataframe,
+#              dataframe_column = NULL,
+#              vocabulary = NULL,
+#              domain = NULL,
+#              standard_concept = NULL,
+#              concept_class = NULL) {
+#         
+#             dataframe_relatives <- 
+#                 pivot_wider_by_relative_level(dataframe = dataframe,
+#                                               concept_id_col = dataframe_column,
+#                                               levels_type = "max",
+#                                               include_count = FALSE)
+#             
+#             
+#             dataframe_relatives2 <-
+#                         dataframe_relatives %>%
+#                         
+#             
+#         
+#         
+#         
+#         
+#     d}
--- log.md
+++ log.md
@@ -5,3 +5,4 @@ Timestamp	Activity Type	Function	File	Details
 2020-06-29 16:51:57	write	write_temp_csv	/var/folders/pg/_nr70_kj7y31yvv84yyy7524sx_q0w/T//RtmpAshkUB/file7e5d1b984404.csv	
 2020-06-29 17:08:23	write	write_temp_csv	/var/folders/pg/_nr70_kj7y31yvv84yyy7524sx_q0w/T//RtmpAshkUB/file7e5d28d08e5d.csv	
 2020-06-29 17:11:58	write	write_temp_csv	/var/folders/pg/_nr70_kj7y31yvv84yyy7524sx_q0w/T//RtmpAshkUB/file7e5d1432909a.csv	
+2020-06-29 19:57:24	write	write_temp_csv	/var/folders/pg/_nr70_kj7y31yvv84yyy7524sx_q0w/T//RtmpVbZmGd/file8b094b14c8e9.csv	
