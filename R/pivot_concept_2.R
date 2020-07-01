2f2135f57688c6d54131c5b309ebce7533965b63
Meera Patel <patelm9@mskcc.org>
2020-06-30 20:02:58 -0400

    add pivot functions

---
+++ R/pivot_concept_2.R
@@ -0,0 +1,62 @@
+#' Get all Loinc System (Specimen) Types for a Lab
+#' @description This function takes a dataframe and mutates an additional column providing the specimen type based on the "Has system" relationship id.
+#' @param concept_id_col The column in dataframe that points to the concept_id. If NULL, defaults to "concept_id".
+#' @param dataframe input data
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @importFrom dplyr rename
+#' @importFrom dplyr left_join
+#' @importFrom dplyr distinct
+#' @export
+
+pivot_concept_2 <-
+    function(dataframe,
+             concept_id_col = NULL,
+             names_from,
+             include_count = TRUE) {
+        
+            names_from <- paste0(names_from, "_2")
+        
+            output <- left_join_relationship(dataframe = dataframe,
+                                             dataframe_column = concept_id_col,
+                                             merge_concept_2 = FALSE) %>%
+                        merge_concepts(into = Concept_2,
+                                       !!names_from,
+                                       suffix = "_2")
+            
+            
+            final_output <-
+                output %>%
+                tidyr::pivot_wider(id_cols = concept_id_1,
+                               names_from = !!names_from,
+                               values_from = Concept_2,
+                               values_fn = list(Concept_2 = function(x) paste(unique(x)[1:250] %>% 
+                                                                                  centipede::no_na(), 
+                                                                              collapse = "\n"))) %>%
+                dplyr::mutate_all(substring, 1, 25000)
+            
+            
+            if (include_count) {
+                
+                final_output_count <-
+                    output %>%
+                    tidyr::pivot_wider(id_cols = concept_id_1,
+                                       names_from = !!names_from,
+                                       values_from = Concept_2,
+                                       values_fn = list(Concept_2 = function(x) length(unique(x)))) %>%
+                    dplyr::rename_at(vars(!(concept_id_1)),
+                                     function(x) paste0(x, " Count"))
+                
+                final_output <- 
+                    dplyr::left_join(final_output,
+                                     final_output_count,
+                                     by = "concept_id_1") 
+                
+                return(final_output)
+                
+            } else {
+                return(final_output)
+            }
+        
+        
+    }
---
+++ R/pivot_relatives.R
@@ -0,0 +1,68 @@
+#' Get all Loinc System (Specimen) Types for a Lab
+#' @description This function takes a dataframe and mutates an additional column providing the specimen type based on the "Has system" relationship id.
+#' @param concept_id_col The column in dataframe that points to the concept_id. If NULL, defaults to "concept_id".
+#' @param dataframe input data
+#' @examples 
+#' Random immunosuppressant concept ids
+#' immunosuppressant_concept_ids <- c("35807335","35807331", "21603616", "21600651", "21605199", "21602723") 
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @importFrom dplyr rename
+#' @importFrom dplyr left_join
+#' @importFrom dplyr distinct
+#' @export
+
+pivot_relative <-
+    function(dataframe,
+             concept_id_col = NULL,
+             names_from,
+             include_count = TRUE) {
+        
+            output <- left_join_all_relatives(dataframe = dataframe,
+                                              id_column = concept_id_col)
+            
+            if (is.null(concept_id_col)) {
+                
+                    concept_id_col <- colnames(dataframe)[1]
+                
+            }
+            
+            #Binding output back
+            output <- 
+                output %>%
+                dplyr::bind_rows() %>%
+                rubix::rename_all_remove("^relative_") %>%
+                chariot::merge_concepts(into = "Relative Concept", !!names_from) %>%
+                dplyr::select(-concept_id, -type, -min_levels_of_separation, -max_levels_of_separation)
+            
+            final_output <-
+            output %>%
+                tidyr::pivot_wider(id_cols = !!concept_id_col,
+                                   names_from = !!names_from,
+                                   values_from = `Relative Concept`,
+                                   values_fn = list(`Relative Concept` = function(x) paste(unique(x)[1:250] %>%
+                                                                                               centipede::no_na(), collapse = "\n"))) %>%
+                dplyr::mutate_all(substring, 1, 25000)
+            
+            if (include_count) {
+                
+                final_output_count <-
+                    output %>%
+                    tidyr::pivot_wider(id_cols = !!concept_id_col,
+                                       names_from = !!names_from,
+                                       values_from = `Relative Concept`,
+                                       values_fn = list(`Relative Concept` = function(x) length(unique(x)))) %>%
+                    dplyr::rename_at(vars(!(!!concept_id_col)),
+                                     function(x) paste0(x, " Count"))
+                
+                final_output <- 
+                    dplyr::left_join(final_output,
+                                     final_output_count,
+                                     by = concept_id_col)
+                    
+                
+                return(final_output)
+            } else {
+                return(final_output)
+            }
+    }
