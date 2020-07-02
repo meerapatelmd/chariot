2dfee6c651dd831aefd2c2ee6d3d56587fb41a41
Meera Patel <patelm9@mskcc.org>
2020-07-01 21:16:06 -0400

    bug fix

--- R/Untitled2.R
+++ R/Untitled2.R
@@ -1,12 +1,12 @@
 # FILES <- list.files(path = "R", pattern = "[.]R$", full.names = TRUE)
 # 
 # for (FILE in FILES) {
-#     
+# 
 #     input <- read_file(FILE)
 #     if (grepl("stingr", input)) {
-#         
+# 
 #         secretary::press_enter()
 #     }
-#     
-#     
+# 
+# 
 # }
