67d763c7de47ef8003433d93445156bef23d8101
Meera Patel <patelm9@mskcc.org>
2020-07-02 20:51:08 -0400

    bug fix

--- R/query_athena.R
+++ R/query_athena.R
@@ -1,7 +1,6 @@
 #' Generic query function
 #' @description This is the low level function that is wrapped for more specific queries. The resultset is cached using the R.cache package
 #' @param sql_statement SQL statement
-#' @importFrom seagull connect_to_local_postgres
 #' @importFrom DBI dbGetQuery
 #' @importFrom secretary typewrite_bold
 #' @export
@@ -10,30 +9,37 @@ query_athena <-
         function(sql_statement, verbose = FALSE, cache_resultset = TRUE, override_cache = FALSE) {
             
             if (cache_resultset) {
-            if (override_cache) {
                 
-                conn <- seagull::connect_to_local_postgres(dbname = "athena")
-                resultset <- DBI::dbGetQuery(conn, statement = sql_statement)
-                cache_query(resultset, key=sql_statement)
-                DBI::dbDisconnect(conn)
-                
-            } else {
-            resultset <- tryCatch(load_cached_query(key=sql_statement),
-                                  error = function(e) NULL)
-                if (is.null(resultset)) {
-                    
-                        conn <- seagull::connect_to_local_postgres(dbname = "athena")
+                    if (override_cache) {
+                        
+                        conn <- connect_athena()
                         resultset <- DBI::dbGetQuery(conn, statement = sql_statement)
                         cache_query(resultset, key=sql_statement)
-                        DBI::dbDisconnect(conn)
-                    
-                } else {
-                    if (verbose) {
+                        dc_athena(conn)
                         
-                            secretary::typewrite_bold("Loading resultset from cache", line_number = 0)
+                    } else {
+                        
+                        resultset <- tryCatch(load_cached_query(key=sql_statement),
+                                              error = function(e) NULL)
+                        
+                        if (is.null(resultset)) {
+                            
+                                conn <- seagull::connect_to_local_postgres(dbname = "athena")
+                                resultset <- DBI::dbGetQuery(conn, statement = sql_statement)
+                                cache_query(resultset, key=sql_statement)
+                                DBI::dbDisconnect(conn)
+                            
+                        } else {
+                            
+                                if (verbose) {
+                                    
+                                        secretary::typewrite_bold("Loading resultset from cache", line_number = 0)
+                                    
+                                }
+                            
+                        }
                     }
-                }
-            }
+                
             } else {
                 
                 conn <- seagull::connect_to_local_postgres(dbname = "athena")
