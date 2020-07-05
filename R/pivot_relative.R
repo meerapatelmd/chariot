520410f5c5aaef51ed1c5471153214507098bd48
Meera Patel <patelm9@mskcc.org>
2020-07-04 22:43:34 -0400

    bug fix

--- R/pivot_relative.R
+++ R/pivot_relative.R
@@ -1,6 +1,6 @@
 #' Pivot all relatives of a set of concepts
 #' @description This function takes a dataframe and mutates an additional column providing the specimen type based on the "Has system" relationship id.
-#' @param concept_id_col The column in dataframe that points to the concept_id. If NULL, defaults to "concept_id".
+#' @param concept_id_column The column in dataframe that points to the concept_id. If NULL, defaults to "concept_id".
 #' @param dataframe input data
 #' @param names_from concept table column to be pivoted on
 #' @examples 
@@ -15,7 +15,7 @@
 
 pivot_relative <-
     function(.data,
-             id_col = NULL,
+             id_column = NULL,
              names_from,
              include_count = TRUE,
              omop = FALSE,
@@ -29,13 +29,13 @@ pivot_relative <-
         }
         
             output <- left_join_relatives(.data = .data,
-                                              .id_column = id_col,
+                                          id_column = id_column,
                                           omop = omop,
                                           omop_schema = omop_schema)
             
-            if (is.null(id_col)) {
+            if (is.null(id_column)) {
                 
-                    id_col <- colnames(.data)[1]
+                    id_column <- colnames(.data)[1]
                 
             }
             
@@ -49,13 +49,13 @@ pivot_relative <-
             
             final_output <-
             output %>%
-                tidyr::pivot_wider(id_cols = !!id_col,
+                tidyr::pivot_wider(id_cols = !!id_column,
                                    names_from = !!names_from,
                                    values_from = `Relative Concept`,
                                    values_fn = list(`Relative Concept` = function(x) paste(unique(x)[1:250] %>%
                                                                                                centipede::no_na(), collapse = "\n"))) %>%
                 dplyr::mutate_all(substring, 1, 25000) %>%
-                dplyr::mutate_at(vars(!!id_col),
+                dplyr::mutate_at(vars(!!id_column),
                                  as.integer)
             
             if (include_count) {
@@ -66,13 +66,13 @@ pivot_relative <-
                                        names_from = !!names_from,
                                        values_from = `Relative Concept`,
                                        values_fn = list(`Relative Concept` = function(x) length(unique(x)))) %>%
-                    dplyr::rename_at(vars(!(!!id_col)),
+                    dplyr::rename_at(vars(!(!!id_column)),
                                      function(x) paste0(x, " Count"))
                 
                 final_output <- 
                     dplyr::left_join(final_output,
                                      final_output_count,
-                                     by = id_col)
+                                     by = id_column)
                     
                 
                 return(final_output)
