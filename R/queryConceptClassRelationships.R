7aab61bb34ebdf8437cc7240a4f1a7eb80c82e33
Meera Patel <patelm9@mskcc.org>
2020-07-21 19:27:16 -0400

    update queryConceptClassRelationships function

--- R/queryConceptClassRelationships.R
+++ R/queryConceptClassRelationships.R
@@ -1,15 +1,15 @@
 #' Query Concept Class Relationships
 #' @description This function retrieves the Subject-Predicate-Object triplet from the CONCEPT_RELATIONSHIP table between 2 vocabularies.
-#' @return A dataframe with 3 columns: 1) concept_class_id of vocabulary_1 with name as "{vocabulary_id_1}_concept_class_id" 2) relationship_id from the CONCEPT_RELATIONSHIP table, and 3) concept_class_id of vocabulary_2 with name as "{vocabulary_id_2}_concept_class_id".
+#' @return A dataframe with 3 columns: 1) concept_class_id of vocabulary_1 with name as "{vocabulary_id_1}_concept_class_id" unless vocabulary_id_2 is NULL, in which case it will be concept_class_id_1 2) relationship_id from the CONCEPT_RELATIONSHIP table, and 3) concept_class_id of vocabulary_2 with name as "{vocabulary_id_2}_concept_class_id" unless vocabulary_id_2 is NULL, in which case it will be concept_class_id_2.
 #' @import SqlRender
 #' @param vocabulary_id_1 single vocabulary_id for the first vocabulary (Subject)
-#' @param vocabulary_id_2 single vocabulary_id for the second vocabulary (Object)
+#' @param vocabulary_id_2 single vocabulary_id for the second vocabulary (Object). If vocabulary_id_2 is NULL, vocabulary_id_2 is set to vocabulary_id_1.
 #' @export
 
 
 queryConceptClassRelationships <-
     function(vocabulary_id_1,
-             vocabulary_id_2,
+             vocabulary_id_2 = NULL,
              schema = NULL) {
 
                         if (is.null(schema)) {
@@ -18,19 +18,9 @@ queryConceptClassRelationships <-
 
                         }
 
-                        base <- system.file(package = "chariot")
-                        path <- paste0(base, "/sql/conceptClassRelationship.sql")
-
-                        #Create prefixes for concept_class_columns
-                        concept_class_id_1 <- paste0(tolower(vocabulary_id_1), "_concept_class_id")
-                        concept_class_id_2 <- paste0(tolower(vocabulary_id_2), "_concept_class_id")
-
-                        sql_statement <-
-                                SqlRender::render(SqlRender::readSql(sourceFile = path),
-                                                  concept_class_id_1 = concept_class_id_1,
-                                                  concept_class_id_2 = concept_class_id_2,
-                                                  vocabulary_id_1 = vocabulary_id_1,
-                                                  vocabulary_id_2 = vocabulary_id_2)
+                        sql_statement <- renderConceptClassRelationships(vocabulary_id_1 = vocabulary_id_1,
+                                                                        vocabulary_id_2 = vocabulary_id_2,
+                                                                        schema = schema)
 
                         resultset <- query_athena(sql_statement = sql_statement)
                         return(resultset)
--- R/renderConceptClassRelationships.R
+++ R/renderConceptClassRelationships.R
@@ -1,5 +1,5 @@
-#' Query Concept Class Relationships
-#' @description This function retrieves the Subject-Predicate-Object triplet from the CONCEPT_RELATIONSHIP table between 2 vocabularies.
+#' Render Concept Class Relationships
+#' @description This function renders the SQL that retrieves the Subject-Predicate-Object triplet from the CONCEPT_RELATIONSHIP table between 2 vocabularies.
 #' @return A dataframe with 3 columns: 1) concept_class_id of vocabulary_1 with name as "{vocabulary_id_1}_concept_class_id" unless vocabulary_id_2 is NULL, in which case it will be concept_class_id_1 2) relationship_id from the CONCEPT_RELATIONSHIP table, and 3) concept_class_id of vocabulary_2 with name as "{vocabulary_id_2}_concept_class_id" unless vocabulary_id_2 is NULL, in which case it will be concept_class_id_2.
 #' @import SqlRender
 #' @param vocabulary_id_1 single vocabulary_id for the first vocabulary (Subject)
