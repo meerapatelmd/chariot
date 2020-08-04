85b783a92b5ece8bcd24c8637973a5cd7bed9f00
Meera Patel <patelmeeray@gmail.com>
2020-08-04 19:21:44 -0400

    add loincSystems data

---
+++ data-raw/load.R
@@ -0,0 +1,5 @@
+## code to prepare `input` dataset goes here
+
+loincSystems <- c('Donor', 'XX', 'Cerebra', 'Peric', 'Body', 'Perito', 'Platelet poor', 'Stool', 'Sediment', 'Gastric', 'Synovial', 'Lymph Node', 'Pleural')
+
+usethis::use_data(loincSystems, overwrite = TRUE)
