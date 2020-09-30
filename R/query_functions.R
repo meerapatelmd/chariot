85c56198387c2c20ff4b4bbc66a3fc608954f294
Meera Patel <patelmeeray@gmail.com>
2020-09-30 02:03:23 -0400

    consolidate ho functions

--- R/query_functions.R
+++ R/query_functions.R
@@ -1225,3 +1225,23 @@ queryVocabularyRelationships <-
                                     sleepTime = sleepTime,
                                     ...)
     }
+
+
+#' Query relationship_id in relationship table
+#' @export
+
+query_relationship_name <-
+        function(phrase, type = c("like", "exact")) {
+
+                if (type == "exact") {
+
+                        query_athena(paste0("SELECT * FROM relationship WHERE LOWER(relationship_name) = '", tolower(phrase), "'"))
+
+                } else if (type == "like") {
+
+                        query_athena(paste0("SELECT * FROM relationship WHERE LOWER(relationship_name) LIKE '%", tolower(phrase), "%'"))
+
+                }
+
+        }
+
