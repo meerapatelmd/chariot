7d5cec7b6214ae7f386e10a11cea1f1502a32e76
Meera Patel <patelmeeray@gmail.com>
2020-09-29 22:22:07 -0400

    add documentation for new functions

--- R/dc_athena.R
+++
@@ -1,9 +0,0 @@
-#' Disconnect Local Athena
-#' @import DatabaseConnector
-#' @export
-
-dc_athena <-
-    function(conn) {
-            .Deprecated("dcAthena")
-        DatabaseConnector::dbDisconnect(conn)
-    }
