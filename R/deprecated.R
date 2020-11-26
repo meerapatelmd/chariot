f52e304a7a48e2d6a0a1dbe96c696348e598d413
Meera <patelm9@mskcc.org>
2020-11-25 22:22:30 -0500

    update function

--- R/deprecated.R
+++ R/deprecated.R
@@ -130,3 +130,27 @@ normalizeToHemOncComponents <-
 
         return(unique(component_concept_ids))
     }
+
+
+
+
+
+#' @export
+
+ids_to_integer <-
+        function(data) {
+
+                .Deprecated()
+
+                data %>%
+                        dplyr::mutate_at(dplyr::vars(contains("concept_id")),
+                                         as.integer)
+
+        }
+
+
+
+
+
+
+
--- R/developer_ohdsi_documentation.R
+++ R/developer_ohdsi_documentation.R
@@ -1,13 +1,20 @@
 #' @noRd
 
 read_cdm_wiki_table <-
-        function(cdmTable) {
+        function() {
 
-                response <- xml2::read_html(paste0("https://github.com/OHDSI/CommonDataModel/wiki/", cdmTable))
+                response <<- xml2::read_html("https://ohdsi.github.io/CommonDataModel/cdm60.html")
 
+                data <-
                 response %>%
                         rvest::html_nodes("table") %>%
-                        rvest::html_table() %>%
-                        purrr::set_names(c(cdmTable, "CONVENTIONS"))
+                        rvest::html_table(fill = TRUE)
+
+                names(data) <-
+                response %>%
+                        rvest::html_nodes("h3") %>%
+                        rvest::html_text()
+
+                data
 
         }
