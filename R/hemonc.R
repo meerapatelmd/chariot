120f340f6f6795c87f6d5befe6e0bf174257e05a
Meera Patel <patelmeeray@gmail.com>
2020-11-28 04:30:43 -0500

    update hemonc functions

--- R/hemonc.R
+++ R/hemonc.R
@@ -269,7 +269,7 @@ ho_lookup_antineoplastics <-
 
         if (verbose) {
 
-            cli::cli_rule(left = "Querying")
+            cli::cat_rule(left = "Querying")
 
         }
 
@@ -313,7 +313,7 @@ deconstruct_hemonc_ids <-
              schema = NULL) {
 
         # If any of the concept_ids are regimens, to get their antineoplastic components
-        input_concept <- queryConceptIds(hemonc_concept_ids)
+        input_concept <- queryConceptId(hemonc_concept_ids)
 
         qa <- input_concept %>%
                  dplyr::filter(!(concept_class_id %in% c("Regimen", "Component")))
@@ -349,4 +349,247 @@ deconstruct_hemonc_ids <-
         }
 
         return(unique(component_concept_ids))
+    }
+
+
+
+#' @title
+#' Look up a HemOnc Regimen
+#' @description
+#' Look up a HemOnc Regimen that Corresponds to the set of HemOnc Components provided by Id. This function takes a set list of HemOnc Components and finds all the HemOnc Regimens that contain these Components in the CONCEPT_RELATIONSHIP table. The resultset is then filtered for the HemOnc Regimens that have the same exact number of Components as the length of the `component_concept_ids` parameter.
+#' @param component_concept_ids integer vector of length 1 or greater of all the Component Concept Ids belonging to any regimen to map to a HemOnc Regimen.
+#' @param ... Additional arguments passed to the query_athena function.
+#' @import magrittr
+#' @import dplyr
+#' @export
+
+ho_lookup_regimen <-
+    function(component_concept_ids,
+             schema = NULL,
+             vocabSchema,
+             writeSchema,
+             conn = NULL,
+             cache_only = FALSE,
+             skip_cache = FALSE,
+             override_cache = FALSE,
+             render_sql = FALSE,
+             verbose = FALSE,
+             sleepTime = 1) {
+
+
+        # component_concept_ids <- c(35803211, 35803383)
+
+        # QA that all component concept ids are Components
+        qa <-
+            queryAthena(sql_statement =
+                            SqlRender::render(
+                            "SELECT *
+                             FROM @vocabSchema.concept c
+                             WHERE c.concept_id IN (@component_concept_ids)
+                                AND c.invalid_reason IS NULL
+                                AND c.concept_class_id <> 'Component';",
+                                    component_concept_ids = component_concept_ids,
+                                    vocabSchema = vocabSchema))
+
+
+        if (nrow(qa) > 0) {
+
+            qa <-
+                qa %>%
+                    mergeLabel(into = "Label",
+                               remove = FALSE) %>%
+                    dplyr::select(Label) %>%
+                    unlist() %>%
+                    paste(collapse = ", ")
+
+            stop(sprintf("Not all `component_concept_ids` are HemOnc Components: %s", qa))
+
+        }
+
+
+        # Query Athena DB for all Regimens associated with the inputted Component Concept Ids
+
+        output <- list()
+        for (i in seq_along(component_concept_ids)) {
+
+            output[[i]] <-
+                queryAthena(conn = conn,
+                           sql_statement =
+                               SqlRender::render(
+                               "WITH component_regimens AS (
+                                        SELECT cr.concept_id_2 AS regimen_i_concept_id
+                                        FROM @vocabSchema.concept c
+                                        LEFT JOIN @vocabSchema.concept_relationship cr
+                                        ON cr.concept_id_1 = c.concept_id
+                                        WHERE c.concept_id IN (@component_concept_id)
+                                            AND cr.relationship_id = 'Antineoplastic of'
+                                            AND cr.invalid_reason IS NULL
+                                ),
+                                regimens AS (
+                                       SELECT DISTINCT cr.concept_id_1 AS regimen_concept_id
+                                       FROM @vocabSchema.concept c
+                                       LEFT JOIN @vocabSchema.concept_relationship cr
+                                       ON c.concept_id = cr.concept_id_1
+                                       WHERE c.vocabulary_id = 'HemOnc'
+                                       AND c.invalid_reason IS NULL
+                                       AND c.concept_class_id = 'Regimen'
+                                       AND cr.invalid_reason IS NULL
+                                       AND cr.relationship_id = 'Has antineoplastic'
+                                       GROUP BY cr.concept_id_1
+                                       HAVING COUNT(cr.concept_id_2) = @component_count
+                                )
+
+                                SELECT t.*
+                                FROM component_regimens cr
+                                INNER JOIN regimens t
+                                ON t.regimen_concept_id = cr.regimen_i_concept_id
+                                ;
+                                ",
+                                    vocabSchema = vocabSchema,
+                                    component_count = length(component_concept_ids),
+                                    component_concept_id = component_concept_ids[i]
+                               ))
+
+        }
+
+        output %>%
+            purrr::reduce(dplyr::inner_join, by = "regimen_concept_id") %>%
+            unlist()
+    }
+
+
+#' Find HemOnc Regimen by Components
+#' @description This function takes a set list of HemOnc Components and finds all the HemOnc Regimens that contain these Components in the CONCEPT_RELATIONSHIP table. The resultset is then filtered for the HemOnc Regimens that have the same exact number of Components as the length of the `component_concept_ids` parameter.
+#' @param component_concept_ids integer vector of length 1 or greater of all the Component Concept Ids belonging to any regimen to map to a HemOnc Regimen.
+#' @param ... Additional arguments passed to the query_athena function.
+#' @import magrittr
+#' @import dplyr
+#' @export
+
+queryHemOncCompToReg <-
+    function(component_concept_ids,
+             schema = NULL,
+             conn = NULL,
+             cache_only = FALSE,
+             skip_cache = FALSE,
+             override_cache = FALSE,
+             render_sql = FALSE,
+             verbose = FALSE,
+             sleepTime = 1) {
+
+        .Deprecated("ho_lookup_regimen")
+
+        # For inputs that are actually regimens, a new set of components is derived.
+        component_concept_ids <-
+            normalizeToHemOncComponents(hemonc_concept_ids = component_concept_ids,
+                                        schema = schema)
+
+        # Get input component count to filter HemOnc Regimens based on their own component_counts
+        input_component_count <- length(component_concept_ids)
+
+        # If any of the concept_ids are regimens, to get their antineoplastic components
+        input_concept <-
+            queryConceptId(component_concept_ids,
+                           schema = schema)
+
+        qa <- input_concept %>%
+            rubix::filter_for(filter_col = concept_class_id,
+                              inclusion_vector = c("Regimen",
+                                                   "Component"),
+                              invert = TRUE)
+
+        if (nrow(qa)) {
+            qaHemOncCompToReg <<- qa
+            stop('input concept ids are not Regimen or Components. See qaHemOncCompToReg for more details.')
+        }
+
+
+        # Query Athena DB for all Regimens associated with the inputted Component Concept Ids
+        sql_statement <-
+            renderHemOncCompToReg(component_concept_ids = component_concept_ids,
+                                  schema = schema)
+
+        Regimens <-
+            queryAthena(sql_statement = sql_statement,
+                        conn = conn,
+                        cache_only = cache_only,
+                        skip_cache = skip_cache,
+                        override_cache = override_cache,
+                        render_sql = render_sql,
+                        verbose = verbose,
+                        sleepTime = sleepTime)
+
+        # Query again to get all of the "Has antineoplastic" relationships to HemOnc Components these Regimens have
+        HasAntineoplastics <- queryHemOncRegToAntineo(regimen_concept_ids = Regimens$regimen_concept_id,
+                                                      schema = schema)
+
+
+
+        # Getting the number of unique HemOnc Components associated with each of the HemOnc Regimens found and then filtering for the length of the input component_concept_ids vector
+        HasAntineoplastics2 <-
+            HasAntineoplastics %>%
+            dplyr::group_by(regimen_concept_id) %>%
+            dplyr::summarize(has_antineoplastic_count = length(unique(has_antineoplastic_concept_id)), .groups = "drop") %>%
+            dplyr::filter(has_antineoplastic_count == input_component_count) %>%
+            dplyr::ungroup() %>%
+            dplyr::select(regimen_concept_id) %>%
+            left_join_concept() %>%
+            dplyr::select(-any_of("regimen_concept_id")) %>%
+            rubix::rename_all_prefix("regimen_")
+
+        #If only 1 or less rows, the function is complete. Otherwise, the outputs need to be filtered another time since now we have all the Regimens that have the exact component count match as the input and have at least 1 of the input components, but does not necessarily have all the components
+        #
+        if (nrow(HasAntineoplastics2) <= 1) {
+            return(HasAntineoplastics2)
+        } else {
+            HasAntineoplastics3 <-
+                HasAntineoplastics %>%
+                dplyr::select(regimen_concept_id,
+                              has_antineoplastic_concept_id) %>%
+                dplyr::group_by(regimen_concept_id) %>%
+                dplyr::summarise_at(vars(has_antineoplastic_concept_id),
+                                    list(has_all_components = ~all(. %in% component_concept_ids),
+                                         component_count = ~length(unique(.)))) %>%
+                dplyr::filter(has_all_components == TRUE,
+                              component_count == input_component_count) %>%
+                dplyr::select(regimen_concept_id) %>%
+                left_join_concept(include_synonyms = F) %>%
+                dplyr::select(-starts_with("regimen")) %>%
+                rubix::rename_all_prefix("regimen_")
+
+            return(HasAntineoplastics3)
+        }
+    }
+
+
+
+
+#' Query a HemOnc Regimen's 'Has antineoplastic' Relationship
+#' @export
+
+queryHemOncRegToAntineo <-
+    function(regimen_concept_ids,
+             schema = NULL,
+             conn = NULL,
+             cache_only = FALSE,
+             skip_cache = FALSE,
+             override_cache = FALSE,
+             render_sql = FALSE,
+             verbose = FALSE,
+             sleepTime = 1) {
+
+        .Deprecated("ho_lookup_antineoplastics")
+
+        sql_statement <-
+            renderHemOncRegToAntineoplastics(regimen_concept_ids = regimen_concept_ids,
+                                             schema = schema)
+
+        queryAthena(sql_statement = sql_statement,
+                    conn = conn,
+                    cache_only = cache_only,
+                    skip_cache = skip_cache,
+                    override_cache = override_cache,
+                    render_sql = render_sql,
+                    verbose = verbose,
+                    sleepTime = sleepTime)
     }
