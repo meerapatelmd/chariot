68c0e7c3c4c57f87d27c15c874fef9e6d37c4f6f
Meera Patel <patelmeeray@gmail.com>
2020-12-12 17:00:27 -0500

    add plot vignette

--- doc/plot-loinc-classification.R
+++ doc/plot-loinc-classification.R
@@ -7,11 +7,31 @@ knitr::opts_chunk$set(
 
 ## ----setup--------------------------------------------------------------------
 library(chariot)
+conn <- chariot::connectAthena()
 
-## -----------------------------------------------------------------------------
-loinc_class_concept <- get_concept(37060596,
-                          vocab_schema = "omop_vocabulary")
+## ----concept_class_obj--------------------------------------------------------
+loinc_class_concept <- get_concept(37052681,
+                          vocab_schema = "omop_vocabulary",
+                          conn = conn)
 
-## -----------------------------------------------------------------------------
+## ----concept_class_obj2-------------------------------------------------------
 loinc_class_concept
+
+## ----preview------------------------------------------------------------------
+preview_loinc_classification(concept_class_obj = loinc_class_concept,
+                     conn = conn)
+
+## ----plot---------------------------------------------------------------------
+temp_html <- tempfile(fileext = ".html")
+plot_loinc_classification(conn = conn,
+                          concept_class_obj = loinc_class_concept,
+                          range = 1:2,
+                          file = temp_html)
+
+## ----display------------------------------------------------------------------
+htmltools::includeHTML(path = temp_html)
+
+## ----final--------------------------------------------------------------------
+unlink(temp_html)
+dcAthena(conn = conn)
 
