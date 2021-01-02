6ed71f85488cafc63b3c853bf793a4a410f1a3df
Meera Patel <patelmeeray@gmail.com>
2021-01-02 14:39:39 -0500

    deploy pkg and GitHub Pages using glitter

--- inst/example/format_merge.R
+++ inst/example/format_merge.R
@@ -1,15 +1,26 @@
 library(tidyverse)
 library(chariot)
 
+conn <- connectAthena()
+# Get a strip for an aspirin concept
 get_strip(concept_id = 1112807,
-         vocab_schema = "omop_vocabulary")
+         vocab_schema = "omop_vocabulary",
+         conn = conn)
 
-test_strip <- tibble::tibble(concept = "[V] [S] 1112807 aspirin [RxNorm 1191] [Drug] [Ingredient]")
+# Unmerge the Strip to get each attribute back
+test_strip <-
+        tibble::tibble(
+                concept = "[V] [S] 1112807 aspirin [RxNorm 1191] [Drug] [Ingredient]"
+                )
 unmerge_strip(test_strip,
               strip_col = concept)
 
+# Merge a dataframe of concepts into stris
 test_data <-
         queryAthena("SELECT * FROM omop_vocabulary.concept ORDER BY RANDOM() LIMIT 20;",
-                    cache_only = TRUE)
+                    conn = conn)
 merge_strip(test_data,
             into = "concept")
+
+
+dcAthena(conn = conn)
