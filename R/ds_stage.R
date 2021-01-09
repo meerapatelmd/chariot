ace29220d0d2e1202a3e8dcdf42d01afbc0d9874
Meera Patel <patelmeeray@gmail.com>
2021-01-09 13:16:18 -0500

    add ds_stage

---
+++ R/ds_stage.R
@@ -0,0 +1,131 @@
+
+
+
+ds_stage_unit_fraction <-
+        function(conn,
+                 conn_fun = "connectAthena()",
+                 write_schema = "patelm9",
+                 verbose = TRUE,
+                 render_sql = TRUE,
+                 render_only = FALSE) {
+
+                sql_statement <-
+                SqlRender::render(
+                "
+                DROP TABLE IF EXISTS @write_schema.ds_unit_fraction;
+                CREATE TABLE @write_schema.ds_unit_fraction (
+                        drug_concept_id INTEGER,
+                	ingredient_concept_id INTEGER,
+                	amount_value NUMERIC,
+                	amount_unit_concept_id INTEGER,
+                	amount_unit_concept_name VARCHAR(255),
+                	numerator_value NUMERIC,
+                	numerator_unit_concept_id INTEGER,
+                	numerator_unit_concept_name VARCHAR(255),
+                	denominator_value NUMERIC,
+                	denominator_unit_concept_id INTEGER,
+                	denominator_unit_concept_name VARCHAR(255),
+                	unit TEXT
+                );
+
+                WITH fraction_unit AS (
+                	SELECT
+                		dsp.*,
+                		CASE
+                			WHEN dsp.numerator_unit_concept_name IS NOT NULL AND dsp.denominator_unit_concept_name IS NOT NULL AND dsp.denominator_unit_concept_name <> dsp.numerator_unit_concept_name
+                				THEN CONCAT(dsp.numerator_unit_concept_name, '/', dsp.denominator_unit_concept_name)
+                			WHEN dsp.numerator_unit_concept_name IS NOT NULL AND dsp.denominator_unit_concept_name IS NULL
+                				THEN dsp.numerator_unit_concept_name
+                		END fraction_unit_concept_name
+                	FROM @write_schema.drug_strength_processed dsp
+                )
+                INSERT INTO @write_schema.ds_unit_fraction
+                SELECT
+                	fu.drug_concept_id,
+                	fu.ingredient_concept_id,
+                	fu.amount_value,
+                	fu.amount_unit_concept_id,
+                	fu.amount_unit_concept_name,
+                	fu.numerator_value,
+                	fu.numerator_unit_concept_id,
+                	fu.numerator_unit_concept_name,
+                	fu.denominator_value,
+                	fu.denominator_unit_concept_id,
+                	fu.denominator_unit_concept_name,
+                	coalesce(fraction_unit_concept_name, amount_unit_concept_name) AS unit
+                FROM fraction_unit fu
+                ;
+                ",
+                write_schema = write_schema)
+
+                sendAthena(conn = conn,
+                           sql_statement = sql_statement,
+                           verbose = verbose,
+                           render_sql = render_sql,
+                           render_only = render_only)
+        }
+
+
+ds_stage_value_fraction <-
+        function(conn,
+                 conn_fun = "connectAthena()",
+                 write_schema = "patelm9",
+                 verbose = TRUE,
+                 render_sql = TRUE,
+                 render_only = FALSE) {
+
+                sql_statement <-
+                        SqlRender::render(
+                                "
+                DROP TABLE IF EXISTS @write_schema.ds_value_fraction;
+                CREATE TABLE @write_schema.ds_value_fraction (
+                        drug_concept_id INTEGER,
+                	ingredient_concept_id INTEGER,
+                	amount_value NUMERIC,
+                	amount_unit_concept_id INTEGER,
+                	amount_unit_concept_name VARCHAR(255),
+                	numerator_value NUMERIC,
+                	numerator_unit_concept_id INTEGER,
+                	numerator_unit_concept_name VARCHAR(255),
+                	denominator_value NUMERIC,
+                	denominator_unit_concept_id INTEGER,
+                	denominator_unit_concept_name VARCHAR(255),
+                	value TEXT
+                );
+
+                WITH fraction_value AS (
+                	SELECT
+                		dsp.*,
+                		CASE
+                			WHEN dsp.numerator_value IS NOT NULL AND dsp.denominator_value IS NOT NULL AND dsp.denominator_value <> dsp.numerator_value
+                				THEN CONCAT(dsp.numerator_value, '/', dsp.denominator_value)
+                			WHEN dsp.numerator_value IS NOT NULL AND dsp.denominator_value IS NULL
+                				THEN dsp.numerator_value::varchar
+                		END fraction_value
+                	FROM patelm9.drug_strength_processed dsp
+                )
+
+                INSERT INTO @write_schema.ds_value_fraction
+                SELECT
+                	fu.drug_concept_id,
+                	fu.ingredient_concept_id,
+                	fu.amount_value,
+                	fu.amount_unit_concept_id,
+                	fu.amount_unit_concept_name,
+                	fu.numerator_value,
+                	fu.numerator_unit_concept_id,
+                	fu.numerator_unit_concept_name,
+                	fu.denominator_value,
+                	fu.denominator_unit_concept_id,
+                	fu.denominator_unit_concept_name,
+                	coalesce(fraction_value, amount_value::varchar) AS value
+                FROM fraction_value fu
+                ;",
+                                write_schema = write_schema)
+
+                sendAthena(conn = conn,
+                           sql_statement = sql_statement,
+                           verbose = verbose,
+                           render_sql = render_sql,
+                           render_only = render_only)
+}
