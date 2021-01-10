b5b907f30ac01938be95d9adb67bfbc5c90edae8
Meera Patel <patelmeeray@gmail.com>
2021-01-10 11:30:22 -0500

    add pivot vignette

--- R/lookup_relationship.R
+++ R/lookup_relationship.R
@@ -1,34 +1,59 @@
+#' @title
+#' Lookup Relationships of a Set of Concepts
+#' @description
+#' Returns the forward relationship with concept attributes
+#' of both sides of the relationship.
+#' @param ... Concept ids.
+#' @param check_validity Check to ensure thet the concept ids provided exist
+#' @seealso
+#'  \code{\link[cli]{cli_rule}}
+#'  \code{\link[rlang]{list2}}
+#'  \code{\link[SqlRender]{render}}
+#' @rdname lookup_relationships
+#' @export
+#' @importFrom cli cli_rule
+#' @importFrom rlang list2
+#' @importFrom SqlRender render
 
 
-
-
-
-
-lookup_relationship <-
-  function(concept_ids,
+lookup_relationships <-
+  function(...,
            check_validity = TRUE,
            conn,
-           vocabSchema,
+           conn_fun = "connectAthena()",
+           vocab_schema = "omop_vocabulary",
            cache_only = FALSE,
            skip_cache = FALSE,
            override_cache = FALSE,
            render_sql = FALSE,
            verbose = FALSE,
            sleepTime = 1) {
+
+    if (missing(conn)) {
+
+      conn <- eval(rlang::parse_expr(conn_fun))
+      on.exit(expr = dcAthena(conn = conn),
+              add = TRUE,
+              after = TRUE)
+    }
+
+
     if (check_validity) {
       if (verbose) {
         cli::cli_rule(left = "Checking Validity")
       }
 
+      concept_ids <- unlist(rlang::list2(...))
+
       sql_statement <-
         SqlRender::render(
           "
                             SELECT *
-                            FROM @vocabSchema.concept c
+                            FROM @vocab_schema.concept c
                             WHERE c.concept_id IN (@concept_ids)
                                     AND c.invalid_reason IS NULL
                             ",
-          vocabSchema = vocabSchema,
+          vocab_schema = vocab_schema,
           concept_ids =  concept_ids
         )
 
@@ -74,15 +99,15 @@ lookup_relationship <-
                                         c2.valid_start_date AS valid_start_date_2,
                                         c2.valid_end_date AS valid_end_date_2,
                                         c2.invalid_reason AS invalid_reason_2
-                                FROM @vocabSchema.concept_relationship cr
-                                LEFT JOIN @vocabSchema.concept c1
+                                FROM @vocab_schema.concept_relationship cr
+                                LEFT JOIN @vocab_schema.concept c1
                                 ON cr.concept_id_1 = c1.concept_id
-                                LEFT JOIN @vocabSchema.concept c2
+                                LEFT JOIN @vocab_schema.concept c2
                                 ON cr.concept_id_2 = c2.concept_id
                                 WHERE cr.concept_id_1 IN (@concept_ids)
                                         AND cr.invalid_reason IS NULL
                                 ",
-        vocabSchema = vocabSchema,
+        vocab_schema = vocab_schema,
         concept_ids = concept_ids
       )
 
@@ -97,33 +122,4 @@ lookup_relationship <-
       verbose = verbose,
       sleepTime = sleepTime
     )
-  }
-
-
-
-
-pivot_relationship <-
-  function(concept_ids,
-           check_validity = TRUE,
-           conn,
-           vocabSchema,
-           cache_only = FALSE,
-           skip_cache = FALSE,
-           override_cache = FALSE,
-           render_sql = FALSE,
-           verbose = FALSE,
-           sleepTime = 1) {
-    output <-
-      lookup_relationship(
-        concept_ids = concept_ids,
-        check_validity = check_validity,
-        conn = conn,
-        vocabSchema = vocabSchema,
-        cache_only = cache_only,
-        skip_cache = skip_cache,
-        override_cache = override_cache,
-        render_sql = render_sql,
-        verbose = verbose,
-        sleepTime = sleepTime
-      )
   }
