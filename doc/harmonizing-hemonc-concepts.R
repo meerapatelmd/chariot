219fa396dd9aaeaa1072af0010b35fbb20fbcb59
Meera Patel <patelmeeray@gmail.com>
2021-03-28 18:34:51 -0400

    deploy pkg using glitter

--- doc/harmonizing-hemonc-concepts.R
+++ doc/harmonizing-hemonc-concepts.R
@@ -4,15 +4,15 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----setup--------------------------------------------------------------------
+## ----setup,message=FALSE------------------------------------------------------
 library(chariot)
 library(tidyverse)
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=FALSE---------------------------------------------
 FOLFOX <- get_concept(concept_id = 35806596)
 Irinotecan <- get_concept(concept_id = 35803130)
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=FALSE---------------------------------------------
 new_components <- 
   ho_reduce_to_components(FOLFOX,
                         Irinotecan)
@@ -20,14 +20,14 @@ new_components <-
 ## -----------------------------------------------------------------------------
 new_components
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=FALSE---------------------------------------------
 new_regimen <- 
   ho_lookup_regimen(new_components$concept_id)
 
 ## -----------------------------------------------------------------------------
 new_regimen
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=F-------------------------------------------------
 Trastuzumab <- get_concept(concept_id = 35803361)
 Trastuzumab
 
@@ -35,17 +35,19 @@ Trastuzumab
 new_component_ids2 <- c(new_components$concept_id, Trastuzumab@concept_id)
 new_component_ids2
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=FALSE---------------------------------------------
 new_regimen2 <- ho_lookup_regimen(new_component_ids2)
 
 ## -----------------------------------------------------------------------------
 new_regimen2
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=F-------------------------------------------------
 new_component_objs2 <- lapply(new_component_ids2,
                               get_concept)
 names(new_component_objs2) <- lapply(new_component_objs2,
                                      function(x) slot(x, "concept_name"))
+
+## -----------------------------------------------------------------------------
 new_component_objs2
 
 ## -----------------------------------------------------------------------------
@@ -56,7 +58,7 @@ for (i in seq_along(new_component_objs2)) {
   names(new_component_permutations)[i] <- new_component_objs2[[i]]@concept_name
 }
 
-## -----------------------------------------------------------------------------
+## ----results='hide',message=F-------------------------------------------------
 new_regimens2 <- list()
 for (i in seq_along(new_component_permutations)) {
   
@@ -67,6 +69,8 @@ names(new_regimens2) <- names(new_component_permutations)
 new_regimens2 <-
   new_regimens2 %>%
   purrr::keep(~ nrow(.) > 0)
+
+## -----------------------------------------------------------------------------
 new_regimens2
 
 ## -----------------------------------------------------------------------------
